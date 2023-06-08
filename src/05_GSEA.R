ad0616c8061cf03840a20558109b7c1630cafead
Siying H <siying.huang-ext@sanofi.com>
2023-06-08 17:11:58 +0200

    + enrichscore calc :sparkles:

--- src/05_GSEA.R
+++ src/05_GSEA.R
@@ -13,17 +13,104 @@
 #'       collapse: no
 #' ---
 #+ setup, include = FALSE
-knitr::opts_knit$set(root_dir = "/mnt/c/Users/e0482362/Work/pathway_analysis/src")
 knitr::opts_chunk$set(echo = T, comment = "", message = F, warning = F, error = F)
 options(width = 100)
-#+ libs
+#+
 library(here)
-#' # Gene Set Enrichment analysis
+library(dplyr)
+source(file.path(here(), "src/utils.R"))
+#+ io
+rst <- readRDS(file.path(here(), "data/de_rst.rds"))
+gene_ids <- readRDS(file.path(here(), "data/gene_id.rds"))
 
+#' # Gene Set Enrichment analysis
+#' ![](https://www.gsea-msigdb.org/gsea/images/GSEA-logo.gif)
+#' The first Gene set enrichment proposed
+#' @moothaPGC1aresponsiveGenesInvolved2003
+#' ![](https://media.springernature.com/full/springer-static/image/art%3A10.1038%2Fng1180/MediaObjects/41588_2003_Article_BFng1180_Fig1_HTML.gif?as=webp)
+#' @subramanianGeneSetEnrichment2005
+
+#' ## Enrichment test
+#'
+#' ![](https://www.ncbi.nlm.nih.gov/core/lw/2.0/html/tileshop_pmc/tileshop_pmc_inline.html?title=Click%20on%20image%20to%20zoom&p=PMC3&id=1266115_zpq0370595180001.jpg)
+#'
+#' I demonstrate the principle of GSEA using the DGEA results.
+#' And I use the same gene sets (S) in the ORA session
+#'
+set.seed(123)
+gs1 <- sample(rst$ID, 10)
+gs2 <- c(head(rst$ID, 5), tail(rst$ID, 5))
+gs3 <- head(rst$ID, 10)
+cat("set1:", gs1, "\nset2:", gs2, "\nset3:", gs3)
+#' The whole ranked list (L)
+#' the whole  list can be ranked based on different parameters here i use t-statistics. it can also be fc, signal to noise ratio.
+#' Sorting the DGEA result by t-statistics
+
+rst <- dplyr::arrange(rst, desc(t)) %>%
+  mutate(gs1 = ID %in% gs1)
+
+t_stat <- setNames(rst$t, rst$ID)
+
+barplot(t_stat,
+  xaxt = "n",
+  xlab = "Ranked T-statistics"
+)
+axis(
+  side = 1, at = which(rst$gs1),
+  col.ticks = "red",
+  labels = F
+)
+
+
+#' ### Calculating Enrichment Score
+#'
+#' > The score is calculated by walking down the list L, increasing a running-sum statistic when we encounter a gene in S and decreasing it when we encounter genes not in S. The magnitude of the increment depends on the correlation of the gene with the phenotype. The enrichment score is the maximum deviation from zero encountered in the random walk; [cite gsea]
+
+#' ![formule_es](https://www.ncbi.nlm.nih.gov/pmc/articles/instance/1266115/equ/M2)
+
+n_s <- length(gs1)
+n_tot <- length(t_stat)
+# method1
+es_neg <- -sqrt(n_s / (n_tot - n_s))
+es_pos <- sqrt( (n_tot - n_s)/ n_s )
+rst <- rst %>%
+  mutate(score_gs1 = if_else(gs1, es_pos, es_neg)) %>%
+  mutate(
+    score_gs1=case_when(row_number()==1 ~0 ,
+    TRUE ~ score_gs1),
+  es_gs1 = cumsum(score_gs1))
+
+
+plot(rst$es_gs1, type = "l", ylab ='Enrichment score')
+abline(h = 0, lty = 2, col = "red")
+#' The max value of the enrichment score is{{max(abs(rst$es_gs1))}}
+
+p_miss <- -1 / (n_tot - n_s)
+nr <- sum(abs(t_stat[gs1]))
+rst <- rst %>%
+  mutate(score2_gs1 = if_else(gs1, abs(t) / nr, p_miss)) %>%
+  mutate(score2_gs1 = case_when(row_number()==1 ~0, TRUE ~ score2_gs1),
+  es2_gs1 = cumsum(score2_gs1))
+head(rst)
+plot(rst$es2_gs1, type = "l", ylab = 'Enrichment Score 2')
+abline(h=0, lty=2, col ='red')
+
+calc_enrich_score(rst, gs3, 'ID','t')
+
+
+#' Kolmogorov-Smirnov Tests
+ks_test_plot(rst, 'ID','t', gs1, 'gs1')
+
+
+
+
+#' ## GSEA using R packages
+/*
 #' <details><summary>Session Info</summary>
 sessionInfo()
 #' </details>
 #+ echo = F, eval = F
 # Markdown --------------------------------------------------------
-# rmarkdown::render('src/06_GSEA.R', output_dir = 'output')
-# knitr::spin('src/06_GSEA.R', format = 'Rmd', knit = F )
+# rmarkdown::render('src/05_GSEA.R', output_dir = 'output')
+# knitr::spin('src/05_GSEA.R', format = 'Rmd', knit = F )
+*/
--- src/utils.R
+++ src/utils.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+library(dplyr)
 volc_plot <- function(data,
            lab_method = "p",
            lab_col = "x",
@@ -62,3 +64,110 @@ volc_plot <- function(data,
 
   }
 
+
+#' ks_test_plot
+#'
+#' Perform Kolmogorov-Smirnov non-parametric test. A warapper of the stat:ks.test(), produce visualization of the tests
+#' "" """"
+#' @param data data frame that contains a column of genesymbol name, fold change.
+#' @param genename str, colname of gene symbol.
+#' @param fc str, colname of fold change, should not be on the log scale.
+#' @param pathway_genes a character vector indicating genes of interest, which should be contained in the data set.
+#' @param pathway_name str, the name to put on the plot.
+#' @param plot logical, if TRUE (default) will return ggplot, otherwise, will retur the result of ks.test.
+#'
+#' @return RETURN_DESCRIPTION
+ks_test_plot <- function(data, genename, fc, pathway_genes,
+                         pathway_name, plot = TRUE) {
+  if ("tbl" %in% class(data[, 1])) {
+    stop("The data cannot be tbl_df, tbl objects")
+  }
+  rank_fc <- arrange(data, .data[[fc]]) %>%
+    select(.data[[genename]], .data[[fc]])
+  idx <- rank_fc[, genename] %in% pathway_genes
+  genelist <- rank_fc[idx, ]
+  ks_rst <- ks.test(genelist[, fc], rank_fc[, fc], exact = T)
+
+  if (!plot) {
+    return(ks_rst)
+  }
+
+  ggplot(rank_fc, aes_string(x = fc)) +
+    stat_ecdf(geom = "step", linetype = "dashed") +
+    stat_ecdf(
+      data = data.frame(genelist), aes_string(x = fc),
+      color = "#6f00ff"
+    ) +
+    geom_hline(
+      yintercept = 0.5,
+      color = "#6f00ff57", linetype = "dashed"
+    ) +
+    labs(
+      x = "Fold change", y = "CDF",
+      title = "Kolmogorov-Smirnov test",
+      subtitle = pathway_name,
+      caption = glue::glue(
+        "P value: ", format(ks_rst$p.value, digits = 4, scientific = T),
+        " D= ", round(unname(ks_rst$statistic), 3)
+      )
+    ) +
+    theme_bw()
+}
+
+
+
+#' Calculate enrichment score
+#'
+#' Calculate enrichment score for GSEA
+#'
+#' @param data data.frame object with DEGA result.
+#' @param gs a string vector of gene set.
+#' @param gid_col string or integer to index the the column in the data frame to match against gene set content. Can be gene symbol, gene ID, etc
+#' @param corr_col string or integer index to indicate the correlation column to be used in the calculation.
+#' @param method methods for calculation, index by the first authors: mootha - Mooth et al, 2003; 'sub' for Subramanian et al 2005
+#' @param plot boolean, if TRUE plot the walking plot.
+#'
+#' @return NULL
+calc_enrich_score <- function(data, gs, gid_col, corr_col,
+method ='sub', plot= TRUE) {
+  n_tot <- nrow(data)
+  n_s <- length(gs)
+  nr <- sum(abs(data[which(data[, gid_col] %in% gs), corr_col]))
+
+  p_hit <- switch(method,
+    mootha = sqrt((n_tot - n_s) / n_s),
+    sub = 1 / nr
+  )
+
+  p_miss <- switch(method,
+    mootha = -sqrt(n_s / (n_tot - n_s)),
+    sub = -1 / (n_tot - n_s)
+  )
+
+  data$gs_idx <- data[, gid_col] %in% gs
+
+  data <- switch(method,
+    mootha = {
+      data %>%
+        mutate(score = if_else(gs_idx, p_hit, p_miss)) %>%
+        mutate(
+          score = case_when(row_number() == 1 ~ 0, TRUE ~ score),
+          es = cumsum(score)
+        )
+    },
+    sub = {
+      data$abs_r <- abs(data[, corr_col])
+      data %>%
+        mutate(score = if_else(gs_idx, abs_r * p_hit, p_miss)) %>%
+        mutate(
+          score = case_when(row_number() == 1 ~ 0, TRUE ~ score),
+          es = cumsum(score)
+        )
+    }
+  )
+  if (plot) {
+    plot(data$es, type = "l", ylab = "Enrichment score")
+    abline(h = 0, lty = 2, col = "red")
+  }
+
+}
