de2431979a9265d46586e964e0e85d50926bf708
Siying H <siying.huang-ext@sanofi.com>
2023-06-09 10:26:14 +0200

    + KS test :sparkles:

--- src/05_GSEA.R
+++ src/05_GSEA.R
@@ -7,7 +7,7 @@
 #' output:
 #'   html_document:
 #'     css: sanofi.css
-#'     code_folding: "hide"
+#'     code_folding: "show"
 #'     toc: yes
 #'     toc_float:
 #'       collapse: no
@@ -37,6 +37,7 @@ gene_ids <- readRDS(file.path(here(), "data/gene_id.rds"))
 #' I demonstrate the principle of GSEA using the DGEA results.
 #' And I use the same gene sets (S) in the ORA session
 #'
+
 set.seed(123)
 gs1 <- sample(rst$ID, 10)
 gs2 <- c(head(rst$ID, 5), tail(rst$ID, 5))
@@ -51,12 +52,19 @@ rst <- dplyr::arrange(rst, desc(t)) %>%
 
 t_stat <- setNames(rst$t, rst$ID)
 
-barplot(t_stat,
-  xaxt = "n",
-  xlab = "Ranked T-statistics"
+barplot(unname(t_stat),
+  ylab = "T-statistics",
+  xlab = 'Rank',
+  col = 'grey',
+  border = NA,
+  axes  = T
 )
 axis(
-  side = 1, at = which(rst$gs1),
+  side = 1, at = seq(1, length(t_stat), 1000),
+  labels = F
+)
+axis(
+  side = 3, at = which(rst$gs1),
   col.ticks = "red",
   labels = F
 )
@@ -65,42 +73,79 @@ axis(
 #' ### Calculating Enrichment Score
 #'
 #' > The score is calculated by walking down the list L, increasing a running-sum statistic when we encounter a gene in S and decreasing it when we encounter genes not in S. The magnitude of the increment depends on the correlation of the gene with the phenotype. The enrichment score is the maximum deviation from zero encountered in the random walk; [cite gsea]
-
-#' ![formule_es](https://www.ncbi.nlm.nih.gov/pmc/articles/instance/1266115/equ/M2)
-
-n_s <- length(gs1)
+#'
+#' Here I use *gene set 1* as an example to illustrate the calculation of enrichment score (ES).
+#'
+#' **Method from *Mootha et al 2003* ** [cite]
+#' $$P_{hit}(S,i)= \sum_{g_{j}{\in}S}{\sqrt{\frac{(N-N_h)}{N_h}}}$$
+#' $$P_{miss}(S,i) =\sum_{g_{j}{\not\in}S}{ \sqrt{\frac{N_h}{(N-N_h)}}}$$
+n_h <- length(gs1)
 n_tot <- length(t_stat)
 # method1
-es_neg <- -sqrt(n_s / (n_tot - n_s))
-es_pos <- sqrt( (n_tot - n_s)/ n_s )
+es_neg <- -sqrt(n_h / (n_tot - n_h))
+es_pos <- sqrt((n_tot - n_h) / n_h)
 rst <- rst %>%
   mutate(score_gs1 = if_else(gs1, es_pos, es_neg)) %>%
   mutate(
-    score_gs1=case_when(row_number()==1 ~0 ,
-    TRUE ~ score_gs1),
+    score_gs1=case_when(row_number()==1 ~0 , TRUE ~ score_gs1),
   es_gs1 = cumsum(score_gs1))
 
-
-plot(rst$es_gs1, type = "l", ylab ='Enrichment score')
-abline(h = 0, lty = 2, col = "red")
-#' The max value of the enrichment score is{{max(abs(rst$es_gs1))}}
-
-p_miss <- -1 / (n_tot - n_s)
+#' The max value of the enrichment score is `r round(max(abs(rst$es_gs1)),2)`
+#'
+#' **Method from *Subramanian et al 2005* **[cite]
+#' $$\begin{equation*}\;P_{{\mathrm{hit}}}(S,i)={{\sum_{\begin{matrix}\scriptstyle{g_{j}{\in}S}\\ \scriptstyle{j{\leq}i}\end{matrix}}}}\frac{|r_{j}|^{p}}{N_{R}},\hspace{1em}{\mathrm{where}}{\;}N_{R}={{\sum_{g_{j}{\in}S}}}|r_{j}|^{p} \end{equation*}$$
+#' $$\begin{equation*}\;P_{{\mathrm{miss}}}(S,i)={{\sum_{\begin{matrix}\scriptstyle{g_{j}{\not\in}S}\\ \scriptstyle{j{\leq}i}\end{matrix}}}}\frac{1}{(N-N_{H})}.\;\end{equation*}$$
+p_miss <- -1 / (n_tot - n_h)
 nr <- sum(abs(t_stat[gs1]))
 rst <- rst %>%
   mutate(score2_gs1 = if_else(gs1, abs(t) / nr, p_miss)) %>%
-  mutate(score2_gs1 = case_when(row_number()==1 ~0, TRUE ~ score2_gs1),
-  es2_gs1 = cumsum(score2_gs1))
-head(rst)
-plot(rst$es2_gs1, type = "l", ylab = 'Enrichment Score 2')
-abline(h=0, lty=2, col ='red')
+  mutate(
+    score2_gs1 = case_when(row_number() == 1 ~ 0, TRUE ~ score2_gs1),
+    es2_gs1 = cumsum(score2_gs1)
+  )
+  #+ walking plot, fig.dim = c(8, 4)
+  par(mfrow = c(1, 2), cex = 0.8)
+  plot(rst$es_gs1, type = "l", ylab = "Enrichment score", main = "ES (Mootha et al)")
+  abline(h = 0, lty = 2, col = "red")
+  axis(
+    side = 3, at = which(rst$gs1),
+    col.ticks = "red",
+    labels = F
+  )
+  plot(rst$es2_gs1, type = "l", ylab = "Enrichment Score", main = "ES score (Subramanian et al 2005)")
+  abline(h = 0, lty = 2, col = "red")
+  axis(
+    side = 3, at = which(rst$gs1),
+    col.ticks = "red",
+    labels = F
+  )
 
-calc_enrich_score(rst, gs3, 'ID','t')
+#' Similarily, I can calculate the ES for gene set 2 and gene set 3 (Figure below). And one can clearly see the extreme pattern in the walking plot due to the way that genes were selected in the sets.
+#'
+#+ fig.dim = c(8,4)
+par(mfrow = c(1, 2), cex = 0.8)
+calc_enrich_score(rst, gs2, "ID", "t", "mootha")
+calc_enrich_score(rst, gs3, "ID", "t", "mootha")
 
+#' ## Estimation of Significance Level of ES
+#' This random walk feature correspond to Kolmogorov–Smirnov-like statistic. In the Subramanian et al method, each step the augmentation is differentially weighted given the correlation with the profile or phenotype.
+#'
+#' ### Kolmogorov-Smirnov Tests
+#'
+#' [K-S test](https://fr.wikipedia.org/wiki/Test_de_Kolmogorov-Smirnov) is designed to find general difference in the cumulative distribution. Here I plot simplied the K-S test of the three gene sets.
+#'
 
-#' Kolmogorov-Smirnov Tests
-ks_test_plot(rst, 'ID','t', gs1, 'gs1')
+p1 <- ks_test_plot(rst, "ID", "t", gs1, "gs1")
+p2 <- ks_test_plot(rst, "ID", "t", gs2, "gs2")
+p3 <- ks_test_plot(rst, "ID", "t", gs3, "gs3")
+ggpubr::ggarrange(p1, p2, p3, ncol = 3)
 
+#' limitations:
+#' ### Permutation test
+#'
+#' ###  Adjustment for Multiple Hypothesis Testing.
+#'
+#' ## The Leading-Edge Subset.
 
 
 
