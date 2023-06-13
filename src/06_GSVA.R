3923e795af4ac55d3c602b46c21598c57f6d8f44
Siying H <siying.huang-ext@sanofi.com>
2023-06-13 15:09:22 +0200

    :sparkles: gsva theory fini!

--- src/06_GSVA.R
+++ src/06_GSVA.R
@@ -20,8 +20,9 @@ library(here)
 library(dplyr)
 library(pheatmap)
 library(GSVA)
+source(file.path(here(), "src/utils.R"))
 #' # Gene set variation analysis (GSVA)
-#' The gene set variation analysis (GSVA) was developed by Hänzelmann et al in 2013, which is a a non-parametric, unsupervised method. @hanzelmannGSVAGeneSet2013
+#' The gene set variation analysis (GSVA) was developed by Hänzelmann et al in 2013, which is a a non-parametric, unsupervised method. @hanzelmannGSVAGeneSet2013 All quoted text are from original paper.
 #'
 #' > GSVA calculates sample-wise gene set enrichment scores as a function of
 #' > genes inside and outside the gene set, analogously to a competitive gene
@@ -29,15 +30,22 @@ library(GSVA)
 #' >  the samples independently of any class label.
 #'
 #' ![](/mnt/c/Users/e0482362/Work/pathway_analysis/figure/gsva.png)
+#' The major steps for GSVA method:
 #'
-
-#+ cache = T
+#' - rank and normalize gene expression levels across samples with CDF function.
+#' - rank the gene expression levels within a sample
+#' - calculate enrichment score (ES) and KS test
+#' - calculate enrichment statistics (GSVA score)
+#'
+#+ cache = T, echo = F
 expr <- readRDS(file.path(here(), "data/expr.rds"))
 rst <- readRDS(file.path(here(), "data/de_rst.rds"))
-# pheno <- readRDS(file.path(here(),'data/pheno.rds'))
+n_bg <- 100
+#' For simlipcity, I choose 5 smokers and 5 non-smokers out of the original
+#' dataset. And for the background gene set, I randomly choose `r n_bg` genes.
+#'
+#+ echo = F
 # data management
-
-# pick 3 smokers , 3 non-smokers
 ctrl_ids <- c("GSM464697", "GSM464699", "GSM464705", "GSM464702", "GSM464715")
 case_ids <- c("GSM464678", "GSM464688", "GSM464683", "GSM464692", "GSM464696")
 
@@ -51,23 +59,22 @@ expr <- expr[!duplicated(expr$IDENTIFIER), ] %>%
 #' ## Gene set selection
 #' Here I select the genes of each set1 in the same manner as before. While for
 #' geneset 2 and 3, I choose up and down-regulated genes only for each set.
-# same gene sets
 set.seed(123)
 gs1 <- sample(rst$ID, 10)
-gs2 <- head(rst[which(rst$logFC > 20 & rst$adj.P.Val < 0.0001), "ID"], 10)
-gs3 <- head(rst[which(rst$logFC < -20 & rst$adj.P.Val < 0.0001), "ID"], 10)
+gs2 <- sample(rst[which(rst$logFC > 20 & rst$adj.P.Val < 0.0001), "ID"], 10)
+gs3 <- sample(rst[which(rst$logFC < -20 & rst$adj.P.Val < 0.0001), "ID"], 10)
 
 cat("set1:", gs1, "\nset2:", gs2, "\nset3:", gs3)
-n_bg <- 50
 #' For the purpose of illustration and simplification, I randomly choose another `r n_bg` genes in the expression dataset as background gene.
+#+ echo = F
 bg <- sample(rst$ID, n_bg)
 gs <- Reduce(union, list(gs1, gs2, gs3, bg))
-length(gs)
-
-#' ## Gene set variation calculation
-#+ hmap, fig.dim = c(4, 8)
 expr_g <- expr[gs, ]
-labels <- data.frame(status = rep(c("control", "case"), each = 5))
+#' ## Gene set variation calculation
+#' Here I use the geneset 1 (randomly selected genes) to illustration the GSVA process.
+#'
+#+ hmap, fig.dim = c(4, 8), eval = F, echo = F
+labels <- data.frame(status = rep(c("non-smokers", "smokers"), each = 5))
 rownames(labels) <- c(ctrl_ids, case_ids)
 grid::grid.newpage()
 h <- pheatmap::pheatmap(expr_g,
@@ -79,19 +86,19 @@ h <- pheatmap::pheatmap(expr_g,
   clustering_distance_rows = "correlation",
   clustering_distance_cols = "correlation",
   clusstering_method = "complete",
-  cluster_rows = F, show_rownames = FALSE,
-  cluster_cols = F, show_colnames = FALSE,
+  cluster_rows = T, show_rownames = FALSE,
+  cluster_cols = T, show_colnames = FALSE,
   silent = TRUE
 )
 grid::grid.draw(h)
 
-#' ### esimate cumulative density function for each gene in a gene set
-#' Here I use the example of gene set 1.
-#+ fig.dim = c(8, 4)
-par(mfrow = c(1,2), cex = 0.8)
+#' ### Density function of gene expression levels
+
+#+ fig.dim = c(8, 4), echo = F
+par(mfrow = c(1, 2), cex = 0.8, font.main = 1)
 plot(density(as.numeric(expr_g[gs1[1], ])),
   ylim = c(0, 2.5), xlim = c(0, 12),
-  main = "Gene expression levels across samples"
+  main <- "PDF of gene expression levels"
 )
 invisible(lapply(seq(2, length(gs1)), function(i) {
   g <- gs1[i]
@@ -105,7 +112,7 @@ legend("topleft", gs1, col = 1:10, lty = 1:10, cex = 0.6, bty = "n")
 # cdf before normaliization
 plot(ecdf(expr_g[gs1[1], ]),
   verticals = T, do.points = F, xlim = c(0, 12),
-  main = "Culmulative density of gene expression levels"
+  main <- "Empricial CDF of gene expression levels"
 )
 invisible(lapply(seq(2, length(gs1)), function(i) {
   g <- gs1[i]
@@ -119,8 +126,8 @@ invisible(lapply(seq(2, length(gs1)), function(i) {
 
 legend("topleft", gs1, col = 1:10, lty = 1:10, cex = 0.6, bty = "n")
 
-#' ## normalizing expr levels
-#' normalizing expression levels with culmulative density function Fx
+#' ## Normalizing gene expression levels
+#' In order to be able to compare the gene expression levels on the same scale for the genes in a gene set. One will need to normalize the expression levels.This is done using culmulative density function $F_x$.
 gd <- apply(expr_g, 1, function(x) {
   f <- ecdf(x)
   f(x)
@@ -129,15 +136,18 @@ gd <- apply(expr_g, 1, function(x) {
   data.frame() %>%
   setNames(colnames(expr_g))
 
-# gd <- apply(gd, 1, function(x) ifelse(x == 1, 0.99, x)) %>% t()
+gd <- apply(gd, 2, function(x) ifelse(x == 1, 0.99, x))
 # normalize gene density
-gene_density <- log(gd / (1 - gd))
+gene_density <- data.frame(log(gd / (1 - gd)))
 
-#' Here I plot the emprical CDF instead of the explicit CDF mentioned in the paper for the purpose of illustration.
-#'
+#' Here I plot the **emprical CDF** instead of the explicit CDF mentioned in the paper for the purpose of illustration. For microarray data or continous data, the kernel for estimating CDF is Gaussian function, whereas in the count data, the kernel for CDF estimation is Poisson function.
+#' ![Gaussian kernel](/mnt/c/Users/e0482362/Work/pathway_analysis/figure/gaus_kernel.png)
+#' ![](/mnt/c/Users/e0482362/Work/pathway_analysis/figure/pois_kernel.png)
 #+ cdf , echo = F, fig.dim = c(8, 4)
-par(mfrow = c(1, 2))
-plot(density(as.numeric(gene_density[1, ])))
+par(mfrow = c(1, 2), cex = 0.8, font.main = 1)
+plot(density(as.numeric(gene_density[1, ])),
+  main = "PDF after normalization"
+)
 invisible(lapply(seq(2, length(gs1)), function(i) {
   g <- gs1[i]
   lines(density(as.numeric(gene_density[g, ])),
@@ -150,8 +160,8 @@ legend("topleft", gs1, col = 1:10, lty = 1:10, cex = 0.6, bty = "n")
 plot(ecdf(as.numeric(gene_density[1, ])),
   verticals = T, do.points = F,
   xlim = c(-10, 10),
-  ylim = c(0,1),
-  main = "Culmulative density of gene expression levels after normalization"
+  ylim = c(0, 1),
+  main = "CDF after normalization"
 )
 
 invisible(lapply(seq(2, length(gs1)), function(i) {
@@ -166,19 +176,19 @@ invisible(lapply(seq(2, length(gs1)), function(i) {
 legend("bottomright", gs1, col = 1:10, lty = 1:10, cex = 0.6, bty = "n")
 
 #' ## Sample-wise enrichment scores
-#' ### compute rank score
-#' To reduce the influence of potential outliers, we first convert $z_{ij}$ to ranks $z(i)j$ for each sample j and normalize further $r_{ij}=|p/2−z_{(i)j}|$ to make the ranks symmetric around zero.
+#' Next, within each sample, the gene expression levels are reordered. To normalize further, a rank score is computated within each sample.
+#'
+#' ### Compute rank score
+#' > To reduce the influence of potential outliers, we first convert $z_{ij}$ to ranks $z(i)j$ for each sample j and normalize further $r_{ij}=|p/2−z_{(i)j}|$ to make the ranks symmetric around zero.
 #'
 sort_sgn_idxs <- apply(gene_density, 2, order, decreasing = TRUE)
 rownames(sort_sgn_idxs) <- rownames(expr_g)
-head(sort_sgn_idxs)
 
 rank_norm <- apply(sort_sgn_idxs, 2, compute_rank_score)
 rownames(rank_norm) <- rownames(sort_sgn_idxs)
 
-#' ## enrichemnent statistics -GSVA score
-#' here i set tau = 1
-head(rank_norm[, 1:3])
+#' ### Enrichement score (ES)
+#' The ES in GSVA is similar to GSVA using Kolmogorov-Smirnov (KS) like random walk statistic. Here I illustrate calculating ES in one sample. I set $\tau = 1$.
 n_tot = nrow(expr_g)
 n_gs = length(gs1)
 
@@ -186,63 +196,39 @@ sample <- rank_norm[, 1, drop = F] %>%
   data.frame() %>%
   tibble::rownames_to_column("ID") %>%
   mutate(gs1 = ID %in% gs1) %>%
-  rename(rscore = GSM464697)
+  rename(rscore = GSM464697) %>%
+    arrange(rscore)
 sum_gs = sum(sample[sample$gs1, "rscore"])
 dnom = 1/ (n_tot - n_gs)
 
 sample <- sample %>%
-  mutate(score = if_else(gs1, abs(rscore) / sum_gs - as.numeric(gs1) * dnom, 0),
-  score_all =  if_else(!gs1, abs(rscore) / sum_gs - as.numeric(gs1) * dnom, 0)) %>%
+  mutate(
+    score = if_else(gs1, abs(rscore) / sum_gs, -as.numeric(!gs1) * dnom)
+  ) %>%
   mutate(
     score = case_when(row_number() == 1 ~ 0, TRUE ~ score),
-    score_all = case_when(row_number() == 1 ~ 0, TRUE ~ score_all),
-    es = cumsum(score),
-    es_all = cumsum(score_all)
+    es = cumsum(score)
   )
+plot(sample$es, type = "l")
+#' #### KS statistics
+ks_test_plot(sample, "ID", "rscore", gs1, "gs1 enrichment in sample 1", xlab = "rank score")
+#' Similarly one can look at the other two pathways
+p2 <- ks_test_plot(sample, "ID", "rscore", gs2, "gs2 enrichment in sample 1", xlab = "rank score")
+p3 <- ks_test_plot(sample, "ID", "rscore", gs3, "gs3 enrichment in sample 1", xlab = "rank score")
+ggpubr::ggarrange(p2, p3, nrow = 1, ncol = 2)
 
-plot(sample$es, type = "s")
-lines(sample$es_all, type = "s")
+#' ### Enrichemnent statistics
+#' In GSVA, two approaches were prposed to turn KS random walk statistics into enrichment statistics,AKA, GSVA score.
+#' #### Classic maximum deviation from 0.
+#' $$ES_{jk}^{max}= \nu_{jk}[arg max_{l = 1..p}(|\nu_{jk}{l}|)]$$
+#'
+max(abs(sample[, "es", drop = F]))
+# plot(sample$es, type = "l")
+# abline(v = which.max(abs(sample$es)), col = "red", lty = 3)
 
-# calculate random walk stat
-
-gset_idxs <- which(rownames(gene_density) %in% gs1)
-gset_idxs <- gs1
-ks_test_Rcode(data.frame(gene_density[, "GSM464697", drop = F]), gset_idxs)
-
-gene.density <- gene_density[, 1, drop = F]
-ks_test_Rcode <- function(gene.density, gset_idxs, tau = 1, make.plot = FALSE) {
-  n.genes <- length(gene.density)
-  n.gset <- length(gset_idxs)
-
-  sum.gset <- sum(abs(gene.density[gset_idxs])^tau)
-
-  dec <- 1 / (n.genes - n.gset)
-
-  sort.idxs <- order(gene.density, decreasing = T)
-  offsets <- sort(match(gset_idxs, sort.idxs))
-
-  last.idx <- 0
-  values <- rep(NaN, length(gset_idxs))
-  current <- 0
-  for (i in seq_along(offsets)) {
-    current <- current + abs(gene.density[sort.idxs[offsets[i]]])^tau / sum.gset - dec * (offsets[i] - last.idx - 1)
-
-    values[i] <- current
-    last.idx <- offsets[i]
-  }
-  check_zero <- current - dec * (n.genes - last.idx)
-  # if(check_zero > 10^-15){
-  # 	stop(paste=c("Expected zero sum for ks:", check_zero))
-  # }
-  if (make.plot) {
-    plot(offsets, values, type = "l")
-  }
-
-  max.idx <- order(abs(values), decreasing = T)[1]
-  mx.value <- values[max.idx]
-
-  return(mx.value)
-}
+#' #### Normalized enrichment score
+#' $$ES_{jk}^{diff} = |ES_{jk}^+|+|ES_{jk}^-|$$
+max(sample[, "es", drop = F], 0) - min(sample[, "es", drop = F], 0)
 
 #' ## GSVA using R package
 
