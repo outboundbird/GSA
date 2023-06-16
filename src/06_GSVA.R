6a54b0d49c28a5264ef311650ee99a10f7aa849c
Siying H <siying.huang-ext@sanofi.com>
2023-06-16 17:17:58 +0200

    + r gsva

--- src/06_GSVA.R
+++ src/06_GSVA.R
@@ -19,10 +19,9 @@ options(width = 100)
 library(here)
 library(dplyr)
 library(pheatmap)
-library(GSVA)
 source(file.path(here(), "src/utils.R"))
 #' # Gene set variation analysis (GSVA)
-#' The gene set variation analysis (GSVA) was developed by *Hänzelmann et al* in 2013, which is a non-parametric, unsupervised method for gene set enrichement analysis. @hanzelmannGSVAGeneSet2013 ^[All quoted text are from original paper.]
+#' The gene set variation analysis (GSVA) was developed by [*Hänzelmann et al*](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3618321/#sec-4title) in 2013, which is a non-parametric, unsupervised method for gene set enrichement analysis. @hanzelmannGSVAGeneSet2013 ^[All quoted text are from original paper.]
 #'
 #' > GSVA calculates sample-wise gene set enrichment scores as a function of
 #' > genes inside and outside the gene set, analogously to a competitive gene
@@ -41,6 +40,8 @@ source(file.path(here(), "src/utils.R"))
 expr <- readRDS(file.path(here(), "data/expr.rds"))
 rst <- readRDS(file.path(here(), "data/de_rst.rds"))
 n_bg <- 100
+#' ## Smoking study
+#'
 #' For simlipcity, I choose 5 smokers and 5 non-smokers out of the original
 #' dataset. And for the background gene set, I randomly choose `r n_bg` genes.
 #'
@@ -56,9 +57,10 @@ expr <- expr[!duplicated(expr$IDENTIFIER), ] %>%
   tibble::column_to_rownames("IDENTIFIER") %>%
   dplyr::select_at(c(ctrl_ids, case_ids)) %>%
   mutate_if(is.numeric, log2)
-#' ## Gene set selection
+#' ### Gene set selection
 #' Here I select the genes of each set1 in the same manner as before. While for
 #' geneset 2 and 3, I choose up and down-regulated genes only for each set.
+#+ cache = T
 set.seed(123)
 gs1 <- sample(rst$ID, 10)
 gs2 <- sample(rst[which(rst$logFC > 20 & rst$adj.P.Val < 0.0001), "ID"], 10)
@@ -66,7 +68,7 @@ gs3 <- sample(rst[which(rst$logFC < -20 & rst$adj.P.Val < 0.0001), "ID"], 10)
 
 cat("set1:", gs1, "\nset2:", gs2, "\nset3:", gs3)
 #' For the purpose of illustration and simplification, I randomly choose another `r n_bg` genes in the expression dataset as background gene.
-#+ echo = F
+#+ echo = F, cache = T
 bg <- sample(rst$ID, n_bg)
 gs <- Reduce(union, list(gs1, gs2, gs3, bg))
 expr_g <- expr[gs, ]
@@ -140,11 +142,11 @@ gd <- apply(gd, 2, function(x) ifelse(x == 1, 0.99, x))
 # normalize gene density
 gene_density <- data.frame(log(gd / (1 - gd)))
 
-#' Here I plot the **emprical CDF** instead of the explicit CDF mentioned in the paper for the purpose of illustration. For microarray data or continous data, the kernel for estimating CDF is Gaussian function, whereas in the count data, the kernel for CDF estimation is Poisson function.
+#' Here I plot the **emprical CDF** instead of the explicit CDF mentioned in the paper for the purpose of illustration. For microarray data or continous data, the kernel for estimating CDF is Gaussian function, whereas in the count data, the kernel for CDF estimation is Poisson function `r emo::ji('fish')`.
 #'
 #' ![Gaussian kernel](images/gaus_kernel.png){width="300"}
 #'
-#' ![Possion kernel](images/pois_kernel.png){width="300" height="86"}
+#' ![Poisson kernel](images/pois_kernel.png){width="300" height="86"}
 #'
 #+ cdf , echo = F, fig.dim = c(8, 4)
 par(mfrow = c(1, 2), cex = 0.8, font.main = 1)
@@ -191,7 +193,8 @@ rank_norm <- apply(sort_sgn_idxs, 2, compute_rank_score)
 rownames(rank_norm) <- rownames(sort_sgn_idxs)
 
 #' ### Enrichement score (ES)
-#' The ES in GSVA is similar to GSVA using Kolmogorov-Smirnov (KS) like random walk statistic. Here I illustrate calculating ES in one sample. I set $\tau = 1$.
+#' The ES in GSVA is similar to GSVA using [two-sample Kolmogorov-Smirnov (KS)](https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test#Two-sample_Kolmogorov%E2%80%93Smirnov_test) like random walk statistic. Here I illustrate calculating ES in one sample. I set $\tau = 1$.
+#+ cache = T
 n_tot = nrow(expr_g)
 n_gs = length(gs1)
 
@@ -212,17 +215,43 @@ sample <- sample %>%
     score = case_when(row_number() == 1 ~ 0, TRUE ~ score),
     es = cumsum(score)
   )
-plot(sample$es, type = "l")
+plot(sample$es,
+  type = "l",
+  xlab = "Gene rank",
+  ylab = "Enrichment score",
+  main = "Random walk in sample 1"
+)
+axis(side = 3, at = which(sample$gs1), label = F, col.ticks = "red")
+
 #' #### KS statistics
-ks_test_plot(sample, "ID", "rscore", gs1, "gs1 enrichment in sample 1", xlab = "rank score")
+#' Here I simplify the test to a classic KS test. Notice the difference in weight at each step from the random walk plot above.
+#+ cache = T
+ks_test_plot(sample, "ID", "rscore",
+  gs1, "gs1 enrichment in sample 1",
+  xlab = "rank score",
+  exact = F,
+  mexclude = T
+)
 #' Similarly one can look at the other two pathways
-p2 <- ks_test_plot(sample, "ID", "rscore", gs2, "gs2 enrichment in sample 1", xlab = "rank score")
-p3 <- ks_test_plot(sample, "ID", "rscore", gs3, "gs3 enrichment in sample 1", xlab = "rank score")
+#+ fig.dim = c(8, 3), cache = T, echo = F
+p2 <- ks_test_plot(sample, "ID", "rscore",
+  gs2, "gs2 enrichment in sample 1",
+  xlab = "rank score",
+  exact = F,
+  mexclude = T
+)
+p3 <- ks_test_plot(sample, "ID", "rscore",
+  gs3, "gs3 enrichment in sample 1",
+  xlab = "rank score",
+  exact = F,
+  mexclude = T
+)
 ggpubr::ggarrange(p2, p3, nrow = 1, ncol = 2)
 
 #' ### Enrichemnent statistics
 #' In GSVA, two approaches were prposed to turn KS random walk statistics into enrichment statistics,AKA, GSVA score.
-#' #### Classic maximum deviation from 0.
+#' > We offer two approaches for turning the KS like random walk statistic into an enrichment statistic (ES) (also called GSVA score), the classical maximum deviation method and a normalized ES. The first ES is the maximum deviation from zero of the random walk of the j-th sample with respect to the k-th gene set:
+#'
 #' $$ES_{jk}^{max}= \nu_{jk}[arg max_{l = 1..p}(|\nu_{jk}{l}|)]$$
 #'
 max(abs(sample[, "es", drop = F]))
@@ -231,10 +260,58 @@ max(abs(sample[, "es", drop = F]))
 
 #' #### Normalized enrichment score
 #' $$ES_{jk}^{diff} = |ES_{jk}^+|+|ES_{jk}^-|$$
+#'
+#' > where $|ES_{jk}^+|$ and $|ES_{jk}^-|$ are the largest positive and negative random walk deviations from zero, respectively, for sample j and gene set k.
 max(sample[, "es", drop = F], 0) - min(sample[, "es", drop = F], 0)
-
+#' ####  Distribution of GSVA score
+#'
+#' Like the distribution in Eenrichment Score in GSEA, the null distribution of first option Eenrichment Statistics in GSVA  is also bimodal.
+#' 
+#' > ![](images/gsva_null.png){width="520"}
+#' > -- Supplemental material
+#'
 #' ## GSVA using R package
+#'
 #' In this section, I demonstrate GSVA using `GSVA` library in R.
+#' The `gsva`function mainly takes expression data, gene set list and calculate enrichment statistics (GSVA score) with two options mentioned above. By default `gsva` and `gaussian` kernel are selected for the GSVA calculation. Other methods are implemented in this function as well (details in the next section).
+library(GSVA)
+gene_sets <- list(g1 = gs1, gs2 = gs2, gs3 = gs3)
+expr_mtx <- as.matrix(expr_g)
+es_gsva <- gsva(
+  expr_mtx,
+  gset.idx.list = gene_sets,
+  mx.diff = F
+)
+
+#' ### Visualize pathway level enrichment in each sample
+#+ fig.dim = c(6, 3), echo = F
+grid::grid.newpage()
+p <- pheatmap::pheatmap(es_gsva,
+  annotation_col = labels,
+  border_color = NA,
+  # breaks = seq(-6,6, length.out = 100),
+  legend_labels = "",
+  main = "",
+  clustering_distance_rows = "correlation",
+  clustering_distance_cols = "correlation",
+  clusstering_method = "complete",
+  cluster_rows = F, show_rownames = T,
+  cluster_cols = F, show_colnames = T,
+  fontsize = 10,
+  fontsize_col = 5,
+  fontsize_row = 15,
+  angle_col = 45,
+  silent = TRUE
+)
+grid::grid.draw(p)
+
+#' ## Other GSVA-like methods
+#'
+#' - ssGSEA @barbieSystematicRNAInterference2009:
+#' - zscore @leeInferringPathwayActivity2008:
+#' - PLAGE @tomfohrPathwayLevelAnalysis2005:
+#'
+
 
 /*
 #' <details><summary>Session Info</summary>
