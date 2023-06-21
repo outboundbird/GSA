04c363c994b6e34360c590c24d5cb461a431503e
Siying H <siying.huang-ext@sanofi.com>
2023-06-21 12:00:02 +0200

    init power analysis

---
+++ src/09_power.R
@@ -0,0 +1,30 @@
+#' ---
+#' title: Power analysis in gene set analysis
+#' subtitle: 'SAR: sar , Study: study'
+#' author:  Siying Huang (E0482362), Biomarker statistics team
+#' date: 'created: 2023-06-20 , updated (`r Sys.Date()`)'
+#' always_allow_html: true
+#' output:
+#'   html_document:
+#'     css: sanofi.css
+#'     code_folding: "hide"
+#'     toc: yes
+#'     toc_float:
+#'       collapse: no
+#' ---
+#+ setup, include = FALSE
+knitr::opts_knit$set(root_dir = "/mnt/c/Users/e0482362/Work/pathway_analysis/src")
+knitr::opts_chunk$set(echo = T, comment = "", message = F, warning = F, error = F)
+options(width = 100)
+#+ libs
+library(here)
+#' # Power analysis in gene set analysis
+#'
+#' [Swason](https://www.biorxiv.org/content/10.1101/186288v2.full)
+#' <details><summary>Session Info</summary>
+sessionInfo()
+#' </details>
+#+ echo = F, eval = F
+# Markdown --------------------------------------------------------
+# rmarkdown::render('src/09_power.R', output_dir = 'output')
+# knitr::spin('src/09_power.R', format = 'Rmd', knit = FALSE)
