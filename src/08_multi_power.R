398851cb820169b0f158a53eb10a28e550d557a9
Siying H <siying.huang-ext@sanofi.com>
2023-06-26 16:16:29 +0200

    update ref

--- src/08_multi_power.R
+++ src/08_multi_power.R
@@ -11,6 +11,7 @@
 #'     toc: yes
 #'     toc_float:
 #'       collapse: no
+#' bibliography: references.bib
 #' ---
 #+ setup, include = FALSE
 knitr::opts_chunk$set(echo = T, comment = "", message = F, warning = F, error = F)
@@ -19,24 +20,24 @@ options(width = 100)
 #' In this section, I talk about the null hypothesis, multiple comparison issue and statistical power for pathway analysis.
 #'
 #' # Null hypothesis
-#' Type of Null hypothesis
+#' ## Competitive null hypothesis
+#'
+#' ## Self-contained null hypothesis
+#'
+#' ## Hybrid null hypothesis
+#'
 #' # Multiplicity issue
 #' Gene set analysis or pathway analysis usually contains multiple pathways or gene sets. This poses a question of adjustment of multiple testing. Conventially, plenty of methods have been developed to address this issue. However the unique character in the pathway analysis is that these pathways are often interrelated to each other. Thus each of the pathway cannot be considered as "independent" entity. This makes us to reconsiderate the way of setting up threshold for controlling false positive results.
 #'
 #' # Statistical power estimation
 #'
 #' ## Reference
-#' [@benjaminiSelectiveInferenceMultiple](https://academic.oup.com/jrsssb/article/76/1/297/7075946)
-#' @panEffectsThresholdChoice2005
-#' 1.Pan KH, Lih CJ, Cohen SN. Effects of threshold choice on biological conclusions reached during analysis of gene expression by DNA microarrays. Proceedings of the National Academy of Sciences [Internet]. 2005 Jun 21 [cited 2023 Jun 16];102(25):8961–5. Available from: https://www.pnas.org/doi/full/10.1073/pnas.0502674102
-
-
-
-
-#' <details><summary>Session Info</summary>
+#' [Selective inference](https://academic.oup.com/jrsssb/article/76/1/297/7075946)
+#'
+/*#' <details><summary>Session Info</summary>
 sessionInfo()
 #' </details>
 #+ echo = F, eval = F
 # Markdown --------------------------------------------------------
 # rmarkdown::render('src/08_multi_power.R', output_dir = 'output')
-# knitr::spin('src/08_multi.R', format = 'Rmd', knit = FALSE)
+# knitr::spin('src/08_multi.R', format = 'Rmd', knit = FALSE)*/
